9695dad622b9eac8fefa092c87e4cfdabbbb6a2e
sodacris <wjxa20152015@gmail.com>
2024-11-26 23:08:29 +0800

    Revert "refactor"
    This reverts commit 8c22bc3ee8077d570985701036a534e269c16efb.

--- pwsh_git/main.py
+++ pwsh_git/main.py
@@ -1,5 +1,4 @@
 import sys
-import re
 import subprocess
 from pathlib import Path
 import argparse
@@ -21,82 +20,40 @@ def get_branches():
     return ret
 
 
-def get_git_files(command):
-    r = subprocess.getoutput(f"git ls-files {command}")
-    r = r.strip().split("\n")
-    r = filter(lambda x: x.strip() != "", r)
-    return list(r)
-
-
-def get_checkout_commands():
-    result = ["checkout"]
-    r2 = re.findall(r"alias\.(.*)=checkout", subprocess.getoutput("git config list"))
-    result.extend(r2)
-    return result
-
-
-def is_checkout_command(c):
-    c = c[4:].lstrip()
-    for i in get_checkout_commands():
-        if c.startswith(i):
-            return True
-
-
-def get_branch_commands():
-    result = ["branch"]
-    r2 = re.findall(r"alias\.(.*)=branch", subprocess.getoutput("git config list"))
-    result.extend(r2)
-    return result
-
-
-def is_branch_command(c):
-    c = c[4:].lstrip()
-    for i in get_branch_commands():
-        if c.startswith(i):
-            return True
-    return False
-
-
-def get_git_commands():
-    r = subprocess.getoutput("git --help")
-    return re.findall(r"   ([a-z]+)", r)
-
-
 def main():
     log.write_text(str(sys.argv), encoding="utf8")
     parser = argparse.ArgumentParser()
     parser.add_argument("--complete", "-C", type=str, help="commands to complete")
     args = parser.parse_args()
-
-    command: str = args.complete
-
-    supported_commands = []
-    for i in get_git_commands():
-        supported_commands.append([i])
-    for i in get_checkout_commands():
+    if args.complete.startswith("git co"):
         branches = get_branches()
-        for branch in branches:
-            supported_commands.append([i, branch])
-
-    files = get_git_files("--exclude-standard --others")
-    files.extend(get_git_files("--modified"))
-    for f in files:
-        supported_commands.append(["add", f])
-
-    normalized_command = re.sub(r" +", "|", command).split("|")
-    nc = normalized_command[1:]  # remove 'git' prefix
-    nc_len_1 = len(nc) - 1
-    supported_commands = list(
-        filter(
-            lambda x: len(x) >= len(nc)
-            and x[:nc_len_1] == nc[:-1]
-            and x[nc_len_1].startswith(nc[nc_len_1]),
-            supported_commands,
+        filter = args.complete[len("git co") :].strip()
+        if len(filter) > 0:
+            for branch in branches:
+                if branch.startswith(filter):
+                    write_output(branch)
+        else:
+            for branch in branches:
+                write_output(branch)
+    elif args.complete.startswith("git add"):
+        files = (
+            subprocess.getoutput("git ls-files --exclude-standard --others")
+            .strip()
+            .split("\n")
         )
-    )
-    supported_commands.sort(key=lambda x: len(x))
-    for s in supported_commands:
-        write_output(" ".join(s[nc_len_1:]))
+        files.extend(
+            subprocess.getoutput("git ls-files --modified").strip().split("\n")
+        )
+        filter = args.complete[len("git add") :].strip()
+        for line in files:
+            if line.strip() == "":
+                continue
+            if len(filter) > 0:
+                if not line.startswith(filter):
+                    continue
+            print(line)
+    else:
+        sys.stderr.write("command not begins with 'git co'\n")
 
 
 if __name__ == "__main__":
